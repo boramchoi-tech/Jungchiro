bef9544e9a0a6a5b01e2b71b1eee1cfd9b0b86e1
boramchoi-tech <tivbl@naver.com>
2020-05-06 01:22:43 +0900

    commit test

--- Jungchiro/src/main/java/com/jungchiro/poli/login/controller/LoginController.java
+++ Jungchiro/src/main/java/com/jungchiro/poli/login/controller/LoginController.java
@@ -29,6 +29,7 @@ public class LoginController {
 		if (loginDto != null) {
 			System.out.println("로그인 성공");
 			session.setAttribute("loginSession", loginDto);
+			System.out.println("테스트");
 			
 			return "main";
 		} else {
