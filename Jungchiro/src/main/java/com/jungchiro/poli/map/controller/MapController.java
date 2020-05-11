e33dca97cabce8fe45a1b8d441f62c9b9f074bcc
boramchoi-tech <tivbl@naver.com>
2020-05-11 21:04:51 +0900

    map commit

--- Jungchiro/src/main/java/com/jungchiro/poli/map/controller/MapController.java
+++ Jungchiro/src/main/java/com/jungchiro/poli/map/controller/MapController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.jungchiro.poli.map.model.dao.PollsDao;
@@ -18,7 +19,7 @@ public class MapController {
 	@Autowired
 	private PollsDao dao;
 	
-	@RequestMapping("/map.do")
+	@RequestMapping(value = "/map.do", method = RequestMethod.GET)
 	public String map() {
 		return "map/map";
 	}
